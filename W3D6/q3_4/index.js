a791afe4799c4dfdce480c4817ae467f4eefb401
Gidiom <87669936+Gidiom@users.noreply.github.com>
2021-08-09 09:51:43 -0500

    modify

--- W3D6/q3_4/index.js
+++ W3D6/q3_4/index.js
@@ -11,20 +11,34 @@ app.use(express.urlencoded({ extended: false }));
 const PRODUCTS = [
     { id: 111, name: 'AAAA', description: 'This is product AAAA', price: 200 },
     { id: 222, name: 'BBBB', description: 'This is product BBBB', price: 300 },
-    { id: 333, name: 'CCCC', description: 'This is product CCCC', price: 400 }]
+    { id: 333, name: 'CCCC', description: 'This is product CCCC', price: 400 }
+    { id: 444, name: 'DDDD', description: 'This is product DDDD', price: 500 }]
 
+    // adding to cart in hard coded way
 const CART = [
     { name: 'AAAA', price: 200, quantity: 3 },
     { name: 'CCCC', price: 400, quantity: 5}]
 
+var prodIndex;
 app.get('/product/:id', (req, res) => {
-    res.render("product", {
-        ...PRODUCTS.find(e => e.id === parseInt(req.params.id))
-    });
+     prodIndex= PRODUCTS.findIndex(pr=>pr.id===parseInt(req.params.id));
+     if(prodIndex<0){
+        res.send("The requested product is not available");
+     }
+     else{
+        res.render("product", {
+            id:PRODUCTS[prodIndex].id,
+            name:PRODUCTS[prodIndex].name,
+            description:PRODUCTS[prodIndex].description,
+            price:PRODUCTS[prodIndex].price
+            });
+     }
 });
 
-app.get('/myCart', (req, res) => {
-    res.render("shoppingCart", {
+app.post('/addToCart', (req, res) => {
+    //dynamically adding products to cart
+    CART.push(PRODUCTS.find(e => e.id === parseInt(req.body.id)));
+    res.render("shoppingcart", {
         products: CART
     });
 });
